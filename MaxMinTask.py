939dd42311cfb5d3ae7398d6a42aab4bfc426394
twinkal14 <127021613+twinkal14@users.noreply.github.com>
2024-01-29 19:02:24 +0530

    Add files via upload

---
+++ MaxMinTask.py
@@ -0,0 +1,11 @@
+#Find maximum and minimum values from user input
+
+x=(input("\n Enter 3 values :"))
+y=x.split(",")
+print(y)
+
+maxnum=max(y)
+print("\n Max num :",maxnum)
+
+minnum=min(y)
+print("\n Min num :",minnum)
